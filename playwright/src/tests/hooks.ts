3d0aa3c0506f5aef07fa37f024aed305c57342e0
Andres Navia <andres.navia@finkargo.com>
2026-06-17 13:09:18 -0500

    fix:flujo con timeout aumentado a 50000

--- playwright/src/tests/hooks.ts
+++ playwright/src/tests/hooks.ts
@@ -1,9 +1,12 @@
 import { After } from '@cucumber/cucumber';
 import { CustomWorld } from './world';
 
-After({ timeout: 15000 }, async function (this: CustomWorld) {
+After({ timeout: 30000 }, async function (this: CustomWorld) {
   if (this.page && !this.page.isClosed()) {
     await this.page.close();
+  }
+  if (this.context) {
+    await this.context.close();
   }
   if (this.browser) {
     await this.browser.close();
